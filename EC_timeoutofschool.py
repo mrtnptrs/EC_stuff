ea97498aaf42e1c6caa0bb492de30d778255657e
Maarten Peters <maartenpeters@hotmail.nl>
2013-10-17 20:03:50 +0200

    Update EC_timeoutofschool.py
    Translated some things to English and also make the script stop when a value is wrong!

--- EC_timeoutofschool.py
+++ EC_timeoutofschool.py
@@ -1,13 +1,14 @@
 #With this programm you can calculate the time you are out of the school at EC Boxmeer.
 
-aantal_uren_invoer = int(input("Tot het hoeveelste uur heb je vandaag? Alleen maar gehele getallen van 1 t/m 9 invoeren!"))
-minirooster = input("Heb je minirooster? (y/n)") #Wel of geen minirooster?
-if minirooster == "y":  #hoe lang in minuten een lesuur duurt bij minirooster of zonder minirooster.
+aantal_uren_invoer = int(input("Until which hour do you have today? Only insert values 1 till 9."))
+minirooster = input("Do you have minirooster (y/n)")
+if minirooster == "y":  #How loang a school hour is with or without minirooster.
     een_uur = 40
 elif minirooster == "n":
     een_uur = 50
 else:
-    print("Ongeldige invoer: geen y of n ingevuld om eventueel minirooster aan te geven. Je zult errors krijgen!")
+    raise SystemExit("Error: wrong input of minirooster detected! Operation will now be cancelled!")
+
 if aantal_uren_invoer == 1 or 2 or 3:
     aantal_uren = aantal_uren_invoer
 
@@ -24,10 +25,11 @@ elif aantal_uren_invoer>5 and aantal_uren_invoer<10 and minirooster == "n":
     aantal_uren = aantal_uren_invoer + (5/5)   #Eventuele kleine en grote pauze meerekenen.
 
 if aantal_uren_invoer>9 or aantal_uren_invoer<1:
-    print("Error: input is too low or too high, You'll get errors!")
+    raise SystemExit("Error: input of numbers of hours is too low or too high, the operation will now be cancelled!")
+
 aantal_minuten = aantal_uren * een_uur
 
-totaal_uren = (aantal_minuten+510)//60  #510 minuten zijn de 8 uur en 30 minuten van wanneer de school begint.
+totaal_uren = (aantal_minuten+510)//60  #510 minutes are the eight hours and 30 minutes from the time when the school begins.
 rest_minuten = (aantal_minuten+510) % 60
 
 eindtijd = str(totaal_uren) + ":" + str(rest_minuten)
