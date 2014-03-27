dd8f4a3beb252e5fe54c8c53c8f17c375d44bf9b
Maarten Peters <maartenpeters@hotmail.nl>
2014-03-27 13:39:18 +0100

    Update EC_timeoutofschool.py
    *Fixed bug with big pause in combination with minirooster.
    *Other minor changes.

--- EC_timeoutofschool.py
+++ EC_timeoutofschool.py
@@ -1,6 +1,6 @@
 #With this programm you can calculate the time you are out of the school at EC Boxmeer.
 
-debug = "False" #Let this program print the hours and some more info.
+debug = "False" #Let this program print the hours and some more info if it's true.
 
 aantal_uren_invoer = int(input("Until which hour do you have today? Only insert values 1 till 9."))
 
@@ -18,7 +18,7 @@ if aantal_uren_invoer == 1 or aantal_uren_invoer == 2 or aantal_uren_invoer == 3
     if debug == "True":
         print(aantal_uren, "  1, 2, 3")
 
-elif aantal_uren_invoer>=4 and aantal_uren_invoer<=5 and minirooster == "y":
+elif aantal_uren_invoer>=4 and aantal_uren_invoer<=6 and minirooster == "y":
     aantal_uren = aantal_uren_invoer + 0.5   #Eventuele kleine pauze meerekenen.
     if debug == "True":
         print(aantal_uren, "  4, 5, y")
@@ -28,11 +28,10 @@ elif aantal_uren_invoer>=4 and aantal_uren_invoer<=5 and minirooster == "n":
     if debug == "True":
         print(aantal_uren, "  4, 5, n")
 
-elif aantal_uren_invoer>=6 and aantal_uren_invoer<=9 and minirooster == "y":
+elif aantal_uren_invoer>6 and aantal_uren_invoer<=9 and minirooster == "y":
     aantal_uren = aantal_uren_invoer + 1.25   #Eventuele kleine en grote pauze meerekenen.
     if debug == "True":
         print(aantal_uren, "  six till nine, n")
-
 
 elif aantal_uren_invoer>=6 and aantal_uren_invoer<=9 and minirooster == "n":
     aantal_uren = aantal_uren_invoer + 1   #Eventuele kleine en grote pauze meerekenen.
@@ -47,7 +46,7 @@ aantal_minuten = aantal_uren * een_uur
 totaal_uren = int((aantal_minuten+510)/60)  #510 minutes are the eight hours and 30 minutes from the time when the school begins.
 rest_minuten = int(aantal_minuten+510) % 60
 
-if rest_minuten == 0: #to let python display times like 11:00 correctly.
+if rest_minuten == 0: #Hack: to let python display times like 11:00 correctly.
     rest_minuten = "00"
 
 eindtijd = str(totaal_uren) + ":" + str(rest_minuten) #The time you're out of school.
